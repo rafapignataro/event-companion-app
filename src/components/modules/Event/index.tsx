88cde98b8cd8380ee17c7c4483035a1b7010048c
Rafael Pignataro <rafapignataro@gmail.com>
2022-06-01 15:47:19 -0300

    feat: Added initial UI Header to project

--- src/components/modules/Event/index.tsx
+++ src/components/modules/Event/index.tsx
@@ -1,5 +1,7 @@
-import { Col, Row } from 'antd';
+import { Col, Row, Button, Typography } from 'antd';
 import dynamic from 'next/dynamic';
+import { ArrowLeftOutlined, ToolOutlined } from '@ant-design/icons';
+
 import LocationProvider from '../../../contexts/location';
 import { Page } from '../../common/Page';
 
@@ -10,19 +12,30 @@ export const Event = () => {
 	return (
 		<Page title="Event">
 			<LocationProvider>
-				<Row style={{ height: '100%' }}>
-					<Col span={24} xl={12}>
-						<MapWithNoSSR
-							showUserLocation={true}
-							mapCornerStart={
-								{ lat: -23.707487913664902, lng: -46.70137882232666, alt: undefined }
-							}
-							mapCornerEnd={
-								{ lat: -23.695954697016138, lng: -46.69165849685668, alt: undefined }
-							}
-						></MapWithNoSSR>
-					</Col>
-				</Row>
+				<MapWithNoSSR
+					showUserLocation={true}
+					mapCornerStart={
+						{ lat: -23.707487913664902, lng: -46.70137882232666, alt: undefined }
+					}
+					mapCornerEnd={
+						{ lat: -23.695954697016138, lng: -46.69165849685668, alt: undefined }
+					}
+				></MapWithNoSSR>
+				<div id="ui-header" style={{ position: 'absolute', zIndex: 2000, width: '100%' }}>
+					<Row justify="space-between" align="middle" style={{ width: '100%' }} >
+						<Col span={4}>
+							<Button type="primary" shape="round" icon={<ArrowLeftOutlined />} size={'large'} />
+						</Col>
+						<Col span={16}>
+							<Typography.Title level={3} style={{ textAlign: 'center', margin: 0 }}>
+								LOLLAPALOOZA
+							</Typography.Title>
+						</Col>
+						<Col span={4}>
+							<Button type="primary" shape="round" icon={<ToolOutlined />} size={'large'} />
+						</Col>
+					</Row>
+				</div>
 			</LocationProvider>
 		</Page>
 	);
