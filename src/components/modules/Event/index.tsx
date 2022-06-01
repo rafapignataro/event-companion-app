b47ad3df13c1a44668a837b793c41dcde16f13ad
Rafael Pignataro <rafapignataro@gmail.com>
2022-06-01 15:51:01 -0300

    feat: Changed UI Header button colors and aligned center

--- src/components/modules/Event/index.tsx
+++ src/components/modules/Event/index.tsx
@@ -21,10 +21,17 @@ export const Event = () => {
 						{ lat: -23.695954697016138, lng: -46.69165849685668, alt: undefined }
 					}
 				></MapWithNoSSR>
-				<div id="ui-header" style={{ position: 'absolute', zIndex: 2000, width: '100%' }}>
+				<div id="ui-header" style={{
+					position: 'absolute',
+					zIndex: 2000,
+					width: '90%',
+					top: '1.5rem',
+					left: '50%',
+					transform: 'translateX(-50%)'
+				}}>
 					<Row justify="space-between" align="middle" style={{ width: '100%' }} >
 						<Col span={4}>
-							<Button type="primary" shape="round" icon={<ArrowLeftOutlined />} size={'large'} />
+							<Button type="default" shape="round" icon={<ArrowLeftOutlined />} size={'large'} />
 						</Col>
 						<Col span={16}>
 							<Typography.Title level={3} style={{ textAlign: 'center', margin: 0 }}>
@@ -32,12 +39,12 @@ export const Event = () => {
 							</Typography.Title>
 						</Col>
 						<Col span={4}>
-							<Button type="primary" shape="round" icon={<ToolOutlined />} size={'large'} />
+							<Button type="default	" shape="round" icon={<ToolOutlined />} size={'large'} />
 						</Col>
 					</Row>
 				</div>
 			</LocationProvider>
-		</Page>
+		</Page >
 	);
 };
 
